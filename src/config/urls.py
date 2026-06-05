fcba4fdb1dee9f611bfb0eb665e44ee1f0e59770
Jan Sellerbeck <henry4711lp@gmail.com>
2026-06-05 16:13:51 +0200

    fix: add custom 404 page

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 from billing.forms import EmailOrUsernameAuthenticationForm
 from billing.views import FirstLaunchLoginView
+from config.views import page_not_found
+
+handler404 = "config.views.page_not_found"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -17,4 +20,5 @@ urlpatterns = [
     ),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", include("billing.urls")),
+    re_path(r"^.*$", page_not_found, name="page-not-found"),
 ]
