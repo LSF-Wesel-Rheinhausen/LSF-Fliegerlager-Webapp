be267ab601cbc3edd6521573098ff746f8c18fbd
Jan Sellerbeck <henry4711lp@gmail.com>
2026-08-05 22:55:16 +0200

    fix(ui): fix mobile bottom nav background, hide top nav on mobile, bump pwa cache

--- src/billing/pwa_views.py
+++ src/billing/pwa_views.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.templatetags.static import static
 
-PWA_CACHE_VERSION = 14
+PWA_CACHE_VERSION = 15
 
 PWA_SURFACES: dict[str, dict[str, str]] = {
     "admin": {
